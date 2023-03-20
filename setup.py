8ddd03b3c1f1893ea4b7e3ff39003e07d69af89b
Avi Asulin <34214569+aviaIguazio@users.noreply.github.com>
2023-03-20 16:23:53 +0200

    Update setup.py
    update version to 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="metrics_gen",
-    version="0.2.1",
+    version="0.2.2",
     author="Avi Asulin",
     author_email="avia@iguazio.com",
     description="Fake deployment and data generator",
